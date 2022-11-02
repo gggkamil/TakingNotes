85a5e99a5406f6e2a60fe4c0a3f69654ce5cbd23
kdebicki1 <kamil.debicki@cfi.pl>
2022-11-02 09:51:22 +0100

    initial

---
+++ Form1.cs
@@ -0,0 +1,75 @@
+using System.Data;
+
+namespace TakingNotes
+{
+    public partial class Form1 : Form
+    {
+
+        DataTable table;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Title", typeof(string));
+            table.Columns.Add("Messeges", typeof(string));
+
+            dataGridView1.DataSource = table;
+
+            //dataGridView1.Columns["Messages"].Visible = false;
+            dataGridView1.Columns["Title"].Width = 240;
+        }
+
+        private void bttNew_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtMessage.Clear();
+        }
+
+        private void bttSave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(txtTitle.Text, txtMessage.Text);
+            txtTitle.Clear();
+            txtMessage.Clear();
+        }
+
+        private void bttRead_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+            if (index > -1)
+            {
+                txtTitle.Text = table.Rows[index].ItemArray[0].ToString();
+                txtMessage.Text = table.Rows[index].ItemArray[1].ToString();
+            }
+        }
+
+        public void bttDelete_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+            table.Rows[index].Delete();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
