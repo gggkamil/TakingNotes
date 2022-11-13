65671d20ed83914e6b4f054227690fae0711dffb
gggkamil <74270259+gggkamil@users.noreply.github.com>
2022-11-13 19:28:41 +0100

    btnNew

--- Program.cs
+++ Program.cs
@@ -3,13 +3,12 @@ namespace TakingNotes
     internal static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+     
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+           
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
